8592401b90712944bdd0323e4475117ace0ee576
bilinc PC <bilin.chen93@gmail.com>
2020-10-19 21:31:50 +0200

    Finished problem 10 in ps5.py

--- ps5/ps5.py
+++ ps5/ps5.py
@@ -247,9 +247,15 @@ def filter_stories(stories, triggerlist):
 	Returns: a list of only the stories for which a trigger in triggerlist fires.
 	"""
 	# TODO: Problem 10
-	# This is a placeholder
-	# (we're just returning all the stories, with no filtering)
-	return stories
+
+	stories_triggered = []
+	for trigger in triggerlist:
+		for story in stories:
+			# Checks if a trigger can fire on a story in the list, if it does save the story in a new list
+			if trigger.evaluate(story):
+				stories_triggered.append(story)
+
+	return stories_triggered
 
 
 
